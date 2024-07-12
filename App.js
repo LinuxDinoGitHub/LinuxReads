0879ffdeaebfc4f05a002f34cfd3df75a9cad8b1
LinuxDinoGitHub <95269147+LinuxDinoGitHub@users.noreply.github.com>
2024-07-12 17:24:42 +0800

    Added button and book component

--- App.js
+++ App.js
@@ -1,20 +1,18 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import {Button} from "./components/Button"
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text>Linux Reads</Text>
+      <Button />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
 });
---
+++ components/Button.js
@@ -0,0 +1,21 @@
+import {React} from "react";
+import { TouchableOpacity, View, StyleSheet , Text} from "react-native";
+
+const Button = () => {
+    return (
+        <TouchableOpacity style={styles.shape}>
+            <View>
+                <Text>+</Text>
+            </View>
+        </TouchableOpacity>
+    )
+}
+
+const styles = StyleSheet.create({
+    shape: {
+        borderRadius: 10,
+    },
+    wrapper: {
+        display: "flex"
+    }
+})
